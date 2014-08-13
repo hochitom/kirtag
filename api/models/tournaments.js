2581831306f7a37e5b4a517c4bc50c4a3680f8a6
hochitom <hochitom@icloud.com>
2014-08-13 07:57:50 +0200

    fixed typo in filename

---
+++ api/models/tournaments.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var troop = require('mongoose-troop');
+
+var Tournaments = new Schema({
+    slug: {
+        index: true,
+        type: String,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+Tournaments.plugin(troop.timestamp);
+Tournaments.plugin(troop.slugify);
+
+module.exports = mongoose.model('Tournaments', Tournaments)
