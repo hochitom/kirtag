00a4aeaf6d8f3cbb2d67ad4bf3f5afa70091a233
hochitom <hochitom@icloud.com>
2014-08-14 07:41:58 +0200

    render templates on clientside

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -5,22 +5,39 @@ var q = require('q');
 // load hbs template
 var templates = {};
 
-if (templates && !templates['events-detail']) {
-    $.get('/templates/events/detail.html').success(function (tmp) {
-        templates['events-detail'] = handlebars.compile(tmp);
-    });
-}
+var getTemplate = function (tmp) {
+    var d = q.deferred();
 
-$('.js-events-list').on('click', 'a', function (e) {
+    if (!templates)
+        templates = {};
+
+    if (!templates[tmp]) {
+        $.get('/templates/' + tmp).success(function (template) {
+            templates[tmp] = handlebars.compile(template);
+            d.resolve(templates[tmp]);
+        });
+    } else {
+        d.resolve(templates[tmp]);
+    }
+
+    return d.promise;
+};
+
+
+
+$(document).on('click', '.js-async', function (e) {
     e.preventDefault();
-    var link = $(this).attr('href');
+    var $this = $(this);
+    var link = $this.attr('href');
+    var data;
 
-    $.get('//127.0.0.1:3000' + link).success(function (data) {
-        console.log(data);
-        console.log(templates['events-detail']);
-        if (templates['events-detail']) {
-            $('#js-view').html(templates['events-detail']({event: data}));
-            history.pushState({}, data.title, link);
-        }
+    $.get('//127.0.0.1:3000' + link)
+    .then(function (d) {
+        data = d;
+        return getTemplate($this.attr('data-tmp'));
+    })
+    .then(function (template) {
+        $('#js-view').html(template({event: data}));
+        history.pushState({}, data.title, link);
     });
 });
